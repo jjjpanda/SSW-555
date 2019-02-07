d9a7c508f658ec6b5a9e0801d37194887d5aaf35
zildj <zildj100@gmail.com>
2019-02-07 14:48:32 -0500

    p2test

---
+++ proj02.py
@@ -0,0 +1,31 @@
+#Herb Zieger
+def printValid(original, level, tag, args, isValid): #print the parsed line in the specified format matching: --> "0 NOTE dates after now \n <-- 0|NOTE|Y|dates after now"
+    print("--> "+original+"\n<-- "+level+"|"+tag+"|"+isValid+"|"+args)
+levelZero = ["NOTE", "HEAD", "TRLR"] #possible tags for level zero
+levelZeroWeird = ["INDI", "FAM"] #exceptions to normal format
+levelOne = ["NAME", "SEX", "FAMC", "FAMS", "HUSB", "WIFE", "CHIL", "BIRT", "DEAT", "MARR", "DIV"] #possible tags for level one
+levelTwo = ["DATE"] #possbile tags for level two
+f = open("testFamily.ged", "r") #open file
+input = f.read().splitlines() #save file as list of individual lines
+for line in input: #More compact, but not as readable:
+    words = line.split() #split lines into lists
+    if ((words[0] is "0" and words[1] in levelZero) or (words[0] is "1" and words[1] in levelOne) or (words[0] is "2" and words[1] in levelTwo)): # check for normal valid
+        printValid(line, words[0], words[1], ' '.join(words[2:]), "Y")
+    elif (words[0] is "0" and words[2] in levelZeroWeird): #check for weird level 0
+        printValid(line, words[0], words[2], words[1], "Y")
+    else: #if it dosen't fit the above condidtions, it is invalid
+        printValid(line, words[0], words[1], ' '.join(words[2:]), "N")
+"""
+for line in input: #elif chain that does the same thing as above, but more readable
+    words = line.split() #split lines into lists
+    if (words[0] is "0" and words[1] in levelZero): #check for normal level 0
+        printValid(line, words[0], words[1], ' '.join(words[2:]), "Y")
+    elif (words[0] is "0" and words[2] in levelZeroWeird): #check for weird level 0
+        printValid(line, words[0], words[2], words[1], "Y")
+    elif (words[0] is "1" and words[1] in levelOne): #check for level 1
+        printValid(line, words[0], words[1], ' '.join(words[2:]), "Y")
+    elif (words[0] is "2" and words[1] in levelTwo): #check for 2
+        printValid(line, words[0], words[1], ' '.join(words[2:]), "Y")
+    else: #if it dosen't fit the above condidtions, it is invalid
+        printValid(line, words[0], words[1], ' '.join(words[2:]), "N")
+"""
