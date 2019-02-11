b431a35bf5c585831ad057ce86d219a21be9e51b
zildj <zildj100@gmail.com>
2019-02-11 10:59:19 -0500

    validlines.txt removed

--- Project03.py
+++ Project03.py
@@ -22,7 +22,7 @@ def gedcom_cleaner(file_name):
                     '1': {'NAME', 'SEX', 'BIRT', 'DEAT', 'FAMC', 'FAMS', 'MARR', 'HUSB', 'WIFE', 'CHIL', 'DIV'},
                     '2': {'DATE'}}
     
-    open("validlines.txt", "w").close()
+    validLines = ""
     gedcom_file = os.path.realpath(file_name)
     try:
         fp = open(gedcom_file, 'r') # Do the risky action of attempting to open a file
@@ -47,10 +47,10 @@ def gedcom_cleaner(file_name):
 
                 # With variables set, check that their combinations are valid
                 if (level in gedcom_rules.keys()) and (tag in gedcom_rules[level]) and (line[1] != 'FAM') and (line[1] != 'INDI'): #also check that 'FAM' and 'INDI' are not on the second position
-                    outFile = open("validlines.txt", "a")
-                    outFile.write(f"{level}|{tag}|{args}\n")
+                    validLines += (f"{level}|{tag}|{args}\n")
                 else: #if combination of characters is not allowed in that order, pring with a 'N' flag
                     continue
+    return validLines
               
 
 class GedcomFile:
@@ -144,72 +144,65 @@ def datetoString(dateInput):
 
 
 
-def gedcom_categorizer(file_name, gedcom):
-    validlines_file = os.path.realpath(file_name)
-    try:
-        fp = open(validlines_file, 'r') # Do the risky action of attempting to open a file
-    except FileNotFoundError:
-        print("can't open", file_name) # If file not found, raise exception
-    else: # If the file is found
-        with fp:
+def gedcom_categorizer(inputString, gedcom):
+    fp = inputString.splitlines()
+    current_id = None #initiate current_id variable that will be used when parser sees an instance initiator
+    nextbirth = False # set a flag that identifies when the following date is for birth (true) or death (false)
+    nextdeath = False
+    nextmarriage = False
+    nextdivorce = False
+    
+    for line in fp:
+        line = line.rstrip('\n\r').split("|") # split lines into a list
 
-            current_id = None #initiate current_id variable that will be used when parser sees an instance initiator
-            nextbirth = False # set a flag that identifies when the following date is for birth (true) or death (false)
-            nextdeath = False
-            nextmarriage = False
-            nextdivorce = False
-            
-            for line in fp:
-                line = line.rstrip('\n\r').split("|") # split lines into a list
-
-                if line[0] == "0" and line[1] == "INDI": # if we identify a new instance of individual, create instance
-                    current_id = line[2]
-                    gedcom.individual[current_id] = Individual(current_id)
+        if line[0] == "0" and line[1] == "INDI": # if we identify a new instance of individual, create instance
+            current_id = line[2]
+            gedcom.individual[current_id] = Individual(current_id)
+        
+        if line[0] == "0" and line[1] == "FAM":
+            current_id = line[2]
+            gedcom.family[current_id] = Family(current_id) # if we identify a new instance of family, create instance
                 
-                if line[0] == "0" and line[1] == "FAM":
-                    current_id = line[2]
-                    gedcom.family[current_id] = Family(current_id) # if we identify a new instance of family, create instance
-                       
-                if line[0] != "0": # if this is level 1 or 2, we are defining an already created instance of individual or family
-                    # add args to instance of Individual or family
-                    if line[1] == "SEX":
-                        gedcom.individual[current_id].setSex(line[2])
-                    elif line[1] == "NAME":
-                        gedcom.individual[current_id].setName(line[2])
-                    elif line[1] == "FAMC":
-                        gedcom.individual[current_id].setFamc(line[2])
-                    elif line[1] == "FAMS":
-                        gedcom.individual[current_id].setFams(line[2])
-                    elif line[1] == "HUSB":
-                        gedcom.family[current_id].setHusband(line[2])
-                    elif line[1] == "WIFE":
-                        gedcom.family[current_id].setWife(line[2])
-                    elif line[1] == "CHIL":
-                        gedcom.family[current_id].setChildren(line[2])
-                    # if any of the following four are present, set the flag so DATE can be assigned to proper tag
-                    elif line[1] == "BIRT":
-                        nextbirth = True
-                    elif line[1] == "DEAT":
-                        nextdeath = True
-                    elif line[1] == "MARR":
-                        nextmarriage = True
-                    elif line[1] == "DIV":
-                        nextdivorce == True
-                    
-                    # set date to appropriate attribute
-                    elif line[1] == "DATE":
-                        if nextbirth:
-                            gedcom.individual[current_id].setBirthday(line[2])
-                            nextbirth = False
-                        elif nextdeath:
-                            gedcom.individual[current_id].setDeathday(line[2])
-                            nextdeath = False
-                        elif nextmarriage:
-                            gedcom.family[current_id].setMarriage(line[2])
-                            nextmarriage = False
-                        elif nextdivorce:
-                            gedcom.family[current_id].setDivorce(line[2])
-                            nextdivorce = False
+        if line[0] != "0": # if this is level 1 or 2, we are defining an already created instance of individual or family
+            # add args to instance of Individual or family
+            if line[1] == "SEX":
+                gedcom.individual[current_id].setSex(line[2])
+            elif line[1] == "NAME":
+                gedcom.individual[current_id].setName(line[2])
+            elif line[1] == "FAMC":
+                gedcom.individual[current_id].setFamc(line[2])
+            elif line[1] == "FAMS":
+                gedcom.individual[current_id].setFams(line[2])
+            elif line[1] == "HUSB":
+                gedcom.family[current_id].setHusband(line[2])
+            elif line[1] == "WIFE":
+                gedcom.family[current_id].setWife(line[2])
+            elif line[1] == "CHIL":
+                gedcom.family[current_id].setChildren(line[2])
+            # if any of the following four are present, set the flag so DATE can be assigned to proper tag
+            elif line[1] == "BIRT":
+                nextbirth = True
+            elif line[1] == "DEAT":
+                nextdeath = True
+            elif line[1] == "MARR":
+                nextmarriage = True
+            elif line[1] == "DIV":
+                nextdivorce == True
+            
+            # set date to appropriate attribute
+            elif line[1] == "DATE":
+                if nextbirth:
+                    gedcom.individual[current_id].setBirthday(line[2])
+                    nextbirth = False
+                elif nextdeath:
+                    gedcom.individual[current_id].setDeathday(line[2])
+                    nextdeath = False
+                elif nextmarriage:
+                    gedcom.family[current_id].setMarriage(line[2])
+                    nextmarriage = False
+                elif nextdivorce:
+                    gedcom.family[current_id].setDivorce(line[2])
+                    nextdivorce = False
 
                 
                     
@@ -217,8 +210,8 @@ def gedcom_categorizer(file_name, gedcom):
 def main():
     mygedcom = GedcomFile()
     
-    gedcom_cleaner("testFamily.ged")
-    gedcom_categorizer("validlines.txt", mygedcom)
+    valid = gedcom_cleaner("testFamily.ged")
+    gedcom_categorizer(valid, mygedcom)
 
     mygedcom.genTables(mygedcom.individual, mygedcom.family)
     
