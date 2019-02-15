409127b8631b125e3e6e29eac06cd1c6f3e07757
SarBH <30393837+SarBH@users.noreply.github.com>
2019-02-15 12:02:54 -0500

    added explanatory docstrings to user stories. Detailed the print err msg

--- userstories/shUserStories.py
+++ userstories/shUserStories.py
@@ -3,25 +3,37 @@ from datetime import datetime
 
 
 def IDatesInFuture(individuals):
-    """ US01: No Date in individuals can happen after current date """
+    """ US01: No Date in individuals can happen after current date.
+        Tests that any given individual has only valid dates.
+                True: Date is valid
+                False: Date is invalid because is in the future
+    """
     for date in [individuals.birthday, individuals.deathday]:
         if date != "N/A":
             if date < datetime.today():
                 return True
             else:
-                print("ERROR: INDIVIDUAL: US01: Date", date, "occurs in the future")
+                print("ERROR: INDIVIDUAL: US01: individual with ID", individuals.id, "contains date", date.date(), "which occurs in the future")
                 return False
+
 def FDatesInFuture(families):
-    """ US01: No Date in a family can happen after current date """
+    """ US01: No Date in a family can happen after current date
+        Tests that any given family has only valid dates.
+                True: Date is valid
+                False: Date is invalid because is in the future
+    """
     for date in [families.marriage, families.divorce]:
         if date != "N/A":
             if date < datetime.today():
                 return True
             else:
-                print("ERROR: FAMILY: US01: Date", date, "occurs in the future")
+                print("ERROR: FAMILY: US01: family with ID", families.id, "contains date", date.date(), "which occurs in the future")
                 return False
 
-def ageGreaterThan(individual): # --- ERROR WHEN DATE IS INVALID, AND THEREFORE IS A STRING ---
+def ageGreaterThan(individual):
+    """ US07: No individual can live more than 150 years.
+            False: Individual lives more than 150 years.
+    """
     if (individual.deathday == "N/A"):
         currentDate = datetime.now()
         age = currentDate.year - individual.birthday.year - ((currentDate.month, currentDate.day) < (individual.birthday.month, individual.birthday.day))
@@ -41,33 +53,4 @@ def test(individuals, families):
         FDatesInFuture(fam)
 
 
-# class SprintOneTest(unittest.TestCase):
-#     """
-#     @I1@ lives longer than 150
-#     @I2@ is born and dies in the future 
-#     """
-#     def test_datesInFuture(self):
-#         """ Tests US01: No Date can happen after current date """
-#         mygedcom = parser.GedcomFile()
-#         valid = parser.gedcom_cleaner("shSprint1Test.ged")
-#         parser.gedcom_categorizer(valid, mygedcom)
-
-#         self.assertTrue(datesInFuture(mygedcom.individual["@I2@"].deathday))
-#         self.assertFalse(datesInFuture(mygedcom.individual["@I1@"].birthday))
-#         self.assertTrue(datesInFuture(mygedcom.individual["@I2@"].birthday))
-
-#     def test_ageGreaterThan(self):
-#         mygedcom = parser.GedcomFile()
-#         valid = parser.gedcom_cleaner("shSprint1Test.ged")
-#         parser.gedcom_categorizer(valid, mygedcom)
-
-#         self.assertTrue(datesInFuture(mygedcom.individual["@I2@"].deathday))
-#         self.assertFalse(datesInFuture(mygedcom.individual["@I1@"].birthday))
-#         self.assertTrue(datesInFuture(mygedcom.individual["@I2@"].birthday))
-   
-
-
-if __name__ == '__main__':
-    # main()
-    unittest.main(exit = False, verbosity = 2)
     
