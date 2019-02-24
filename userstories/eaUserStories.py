f1fb3254ef4e898453d1ea431db15f06805113ce
esethral <evanabel311@gmail.com>
2019-02-24 18:57:35 -0500

    User stories 2 & 3

--- userstories/eaUserStories.py
+++ userstories/eaUserStories.py
@@ -1,6 +1,29 @@
 import sys
 sys.dont_write_bytecode = True
+import unittest
+import datetime
+
+def birthBeforeMarriage(individual, family):
+    if family.marriage == "N/A":
+        return True
+
+    if individual.birthday > family.marraige:
+        print(f"ERROR: (US02) Individual ({individual.id}) was married before they were born")
+        return False
+    else:
+        return True
+
+def birthBeforeDeath(individual):
+    if individual.deathday == "N/A":
+        return True
+
+    if individual.birthday > individual.deathday:
+        print(f"ERROR: (US03) Individual ({individual.id}) died before they were born")
+        return False
+    else:
+        return True
 
 def main(individuals, families):
-    print("")
-    
+    for person in individuals.values():
+        birthBeforeMarriage(person, families[person.fams])
+        birthBeforeDeath(person)
